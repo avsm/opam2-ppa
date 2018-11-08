bb68d717ce1e83e1a11180c4c6ef7ddfe0de72cb
Anil Madhavapeddy <anil@recoil.org>
2018-11-08 15:38:55 +0000

    port to opam 2.0.1
    closes #1, thanks to testing by @frank-emrich
    
    Signed-off-by: Anil Madhavapeddy <anil@recoil.org>

--- scripts/container-build.sh
+++ scripts/container-build.sh
@@ -2,20 +2,21 @@
 
 set -ex
 
-targ=avsm/ppa-opam-experimental
-ppa="ppa5"
+targ=avsm/ppa
+ppa="ppa6"
+opamver="2.0.1"
 
 for distro in bionic cosmic; do
-  v="2.0.0-0$ppa~$distro"
+  v="${opamver}-0$ppa~$distro"
   file="opam_$v.orig.tar.gz"
-  cp "opam_2.0.0.orig.tar.gz" $file
-  rm -rf opam-full-2.0.0
+  cp "opam_${opamver}.orig.tar.gz" $file
+  rm -rf opam-full-${opamver}
   tar -zxf $file
-  cp -r overlay/* opam-full-2.0.0/debian/
-  sed -e "s/%%DISTRO%%/$distro/g" -e "s/%%PPA%%/$ppa/g" overlay/debian/changelog.in > opam-full-2.0.0/debian/changelog
-  rm -f opam-full-2.0.0/debian/changelog.in
-  cd opam-full-2.0.0
+  cp -r overlay/* opam-full-${opamver}/debian/
+  sed -e "s/%%DISTRO%%/$distro/g" -e "s/%%PPA%%/$ppa/g" overlay/debian/changelog.in > opam-full-${opamver}/debian/changelog
+  rm -f opam-full-${opamver}/debian/changelog.in
+  cd opam-full-${opamver}
   debuild -S -sa
   cd ..
-  dput ppa:${targ} opam_2.0.0-0${ppa}~${distro}_source.changes
+  dput ppa:${targ} opam_${opamver}-0${ppa}~${distro}_source.changes
 done
